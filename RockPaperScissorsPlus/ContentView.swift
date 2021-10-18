e136b6528ccd8fe0b9a3ca8249ec771fe57a605b
gv4383 <gregory.vargas18@gmail.com>
2021-10-18 00:05:07 -0500

    Initial Commit

---
+++ RockPaperScissorsPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissorsPlus
+//
+//  Created by Goyo Vargas on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissorsPlus/RockPaperScissorsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsPlusApp.swift
+//  RockPaperScissorsPlus
+//
+//  Created by Goyo Vargas on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
