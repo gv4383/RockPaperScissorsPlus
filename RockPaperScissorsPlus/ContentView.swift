538666ff0a352944a289fc5fbadd9b7059fb0692
gv4383 <gregory.vargas18@gmail.com>
2021-10-22 22:14:09 -0500

    Create basic game layout

--- RockPaperScissorsPlus/ContentView.swift
+++ RockPaperScissorsPlus/ContentView.swift
@@ -7,10 +7,73 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MoveButton: View {
+    let moveName: String
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            print("You selected \(moveName)!")
+        }) {
+            Text("\(moveName)")
+                .padding()
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var moves = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ]
+    @State private var outcome = [
+        "Win",
+        "Lose"
+    ]
+    @State private var selectedComputerMove = Int.random(in: 0...2)
+    @State private var desiredOutcome = Int.random(in: 0...1)
+    @State private var showResults = false
+    @State private var resultsDescription = ""
+    
+    var body: some View {
+        VStack{
+            Text("\(outcome[desiredOutcome])")
+            
+            Text("against")
+            
+            Text("\(moves[selectedComputerMove]):")
+            
+            ForEach(0..<moves.count) { move in
+//                TODO: Figure out how to pass in functions
+//                MoveButton(moveName: moves[move])
+                Button(action: {
+                    self.moveTapped(move)
+                }) {
+                    Text("\(moves[move])")
+                        .padding()
+                }
+            }
+        }
+        .alert(isPresented: $showResults) {
+            Alert(
+                title: Text("Test Title"),
+                message: Text(resultsDescription),
+                dismissButton: .default(Text("Continue")) {
+                    self.restartGame()
+                }
+            )
+        }
+    }
+    
+    func moveTapped(_ move: Int) {
+        print("You selected \(moves[move])!")
+        resultsDescription = "Results!"
+        showResults = true
+    }
+    
+    func restartGame() {
+        selectedComputerMove = Int.random(in: 0...2)
+        desiredOutcome = Int.random(in: 0...1)
     }
 }
 
